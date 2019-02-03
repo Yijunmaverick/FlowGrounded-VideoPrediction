e013161833ceea0cf1a2ee666c731b148fbea399
Yijun Li <liyjcse@gmail.com>
2019-02-02 17:11:40 -0800

    Add files via upload

--- download_models.sh
+++ download_models.sh
@@ -6,9 +6,11 @@ cd ../..
 
 cd logs/DTexture/flow_prediction/
 wget -c http://vllab1.ucmerced.edu/~yli62/VideoPrediction/flag/flow_prediction/model_flag_eccv.t7
+wget -c http://vllab1.ucmerced.edu/~yli62/VideoPrediction/kth/flow_prediction/model_kth_eccv.t7
 
 cd ..
 cd flow2rgb/
 wget -c http://vllab1.ucmerced.edu/~yli62/VideoPrediction/flag/flow2rgb/model_flag_eccv.t7
+wget -c http://vllab1.ucmerced.edu/~yli62/VideoPrediction/kth/flow2rgb/model_kth_eccv.t7
 
 cd ../../../..
